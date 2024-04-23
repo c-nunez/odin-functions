b3a33508e619c747bdabf683e165d0bb35edf2b7
c-nunez <nunez_cr@outlook.com>
2024-04-23 12:11:02 -0400

    added documentation in notes

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
+//generate random number between 0 and 100
 function genRanNum() {
   return Math.round(Math.random() * 100);
 }
 
+//provides the sum of 2 random generated numbers
 function sum(num1, num2) {
   return num1 + num2;
 }
+
+//provides product of 2 random generated numbers
 function product(num1, num2) {
   return num1 * num2;
 }
 
+//uses random number generator to produce 2 numbers and displays formula with invoked function for respective calculation
 function updateSum() {
   let num1 = genRanNum();
   let num2 = genRanNum();
@@ -21,15 +26,18 @@ function updateSum() {
   ).innerHTML = `${num1} x ${num2} = ${product(num1, num2)} `;
 }
 
+//finds the last letter in a string
 function findLast() {
   let sentence = document.getElementById("phrase").innerHTML;
   return sentence.at(-1);
 }
 
+//manipulates DOM to display the result of above formula
 function lastLetter() {
   document.getElementById("last").innerHTML = `${findLast()}`;
 }
 
+//recieves input from users and returns uppercase in first letter/word only
 function capWords() {
   let btn = document.getElementById("btnCheck");
   btn.addEventListener("click", function () {
